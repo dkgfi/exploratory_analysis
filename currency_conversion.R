aef2a3c13e27a481378e0f983c9ccbdf0833a40b
margaretmf <margaretmfurr@gmail.com>
2017-03-25 20:26:08 -0400

    Update currency_conversion.R

--- currency_conversion.R
+++ currency_conversion.R
@@ -20,20 +20,17 @@ trade_data <- do.call(rbind,lapply(filenames[grep("trade",filenames)],read.csv))
 # unique currencies
 unique(trade_data$currency) # EUR, JPY, USD
 
-# specific country files - ethiopia
+# specific country files - ethiopia, netherlands, south africa, usa
 ethiopia <- read.csv(filenames[grep("ethiopia",filenames)])
-# specific country files - netherlands
 netherlands <- read.csv(filenames[grep("netherlands",filenames)])
-# specific country files - south africa
 south_africa <- read.csv(filenames[grep("south_africa",filenames)])
-# specific country files - usa
 usa <- read.csv(filenames[grep("usa",filenames)])
 
 # currency
-currency <- read.csv(filenames[3])
+currency <- read.csv(filenames[grep("currency",filenames])
 currency <- currency[order(currency$id),]
 # exchange rate
-exchange_rate <- read.csv(filenames[7])
+exchange_rate <- read.csv(filenames[grep("exchange_rate",filenames])
 exchange_rate <- exchange_rate[order(exchange_rate$currency_id),]exchange_rate[exchange_rate$currency_id%in%currency$id[currency$code%in%c("EUR","JPY")],]
 # merge exchange rate with currency 
 currency_convers <- merge(exchange_rate,currency[,c(1,3)],by.x="currency_id",by.y="id")[,c(1,3:4,6)]
@@ -45,34 +42,28 @@ conversion <- function(x) {
                                                              x$month[x$currency%in%c("EUR","JPY")]),],currency_convers,
         by.x=c("currency","month"),by.y=c("code","month"),all.x=TRUE,all.y=FALSE)
 }
-# conversions - ethiopia
-ethiopia_convers <- conversion(ethiopia)
-ethiopia_convers$value <- ethiopia_convers$value*ethiopia_convers$rate
-# conversions - netherlands
-netherlands_convers <- conversion(netherlands)
-netherlands_convers$value <- netherlands_convers$value*netherlands_convers$rate
-# conversions - south_africa
-south_africa_convers <- conversion(south_africa)
-south_africa_convers$value <- south_africa_convers$value*south_africa_convers$rate
-# conversions - usa
-usa_convers <- conversion(usa)
-usa_convers$value <- usa_convers$value*usa_convers$rate
-# conversions - trade
-trade_convers <- conversion(trade_data)
-trade_convers$value <- trade_convers$value*usa_convers$rate
-
 # bind
 bind <- function(x,x_convers) {
   rbind(x[x$currency%in%c("USD"),],x_convers[,1:ncol(x)])
 }
-# bind - ethiopia
+# conversions & bind - ethiopia
+ethiopia_convers <- conversion(ethiopia)
+ethiopia_convers$value <- ethiopia_convers$value*ethiopia_convers$rate
 ethiopia <- bind(ethiopia,ethiopia_convers)
-# bind - netherlands
+# conversions - netherlands
+netherlands_convers <- conversion(netherlands)
+netherlands_convers$value <- netherlands_convers$value*netherlands_convers$rate
 netherlands <- bind(netherlands,netherlands_convers)
-# bind - south_africa
+# conversions - south_africa
+south_africa_convers <- conversion(south_africa)
+south_africa_convers$value <- south_africa_convers$value*south_africa_convers$rate
 south_africa <- bind(south_africa,south_africa_convers)
-# bind - usa
+# conversions - usa
+usa_convers <- conversion(usa)
+usa_convers$value <- usa_convers$value*usa_convers$rate
 usa <- bind(usa,usa_convers)
-# bind - trade
+# conversions - trade
+trade_convers <- conversion(trade_data)
+trade_convers$value <- trade_convers$value*usa_convers$rate
 trade_data <- bind(trade_data,trade_convers)
 
